10bf5efc0e0af7dbcdb5410b793f537e26167bc2
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2024-06-01 23:18:30 +0900

    reset error state

--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -48,28 +48,3 @@ export default userReducer;
 
 
 
-// import * as types from "../constants/user.constants";
-// const initialState = {
-//   error: null,
-// };
-
-// const userReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case types.REGISTER_USER_FAIL:
-//       return {
-//         ...state,
-//         error: action.payload,
-//       };
-//     case types.REGISTER_USER_SUCCESS:
-//       return {
-//         ...state,
-//         error: null,
-//       };
-//     // 기타 케이스...
-//     default:
-//       return state;
-//   }
-// };
-
-// export default userReducer;
-
